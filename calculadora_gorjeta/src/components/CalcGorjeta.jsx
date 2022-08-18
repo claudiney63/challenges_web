107202ec80dcb2503bbcadde40543908923588cf
claudiney63 <claudineyryan63@gmail.com>
2022-08-17 23:38:14 -0300

    inicio regra de negocio, calculo da gorjeta

--- calculadora_gorjeta/src/components/CalcGorjeta.jsx
+++ calculadora_gorjeta/src/components/CalcGorjeta.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
@@ -10,25 +12,37 @@ import Typography from '@mui/material/Typography';
 import './CalcGorjeta.css'
 
 export default function CalcGorjeta() {
+
+    const [valor, setValor] = useState(0.00)
+
+    const calcularGorjeta = (comandaValue, gorjetaValue) => {
+        //implementar regra de negocio para calcular gorjeta
+    }
+
     return (
         <>
             <Container maxWidth="sm">
                 <Grid container spacing={4}>
                     <Grid item xs={12}>
-                        <Card sx={{ minWidth: 275, marginTop: 25, backgroundColor: '#e0e0e0'}}>
+                        <Card sx={{ minWidth: 275, marginTop: 25, backgroundColor: '#e0e0e0' }}>
                             <Box sx={{ backgroundColor: 'primary.main', height: 60, width: 575 }}>
                                 <Typography variant="h5" component="div" sx={{ padding: 2, color: 'white' }}>
                                     Calculadora de Gorjeta
                                 </Typography>
                             </Box>
                             <CardContent>
-                                <div>
-                                    <TextField id="outlined-basic" label="Comanda" margin='normal' variant="outlined" type='number' />
-                                </div>
-                                <div>
-                                    <TextField id="outlined-basic" label="Gorjeta" margin='normal' variant="outlined" type='number' />
-                                </div>
-                                <div><Button variant="contained">Calcular</Button></div>
+                                <Grid container spacing={1}>
+                                    <Grid item xs={7}>
+                                            <TextField id="outlined-basic" label="Comanda" margin='normal' variant="outlined" type='number' value={0}/>
+                                            <TextField id="outlined-basic" label="Gorjeta" margin='normal' variant="outlined" type='number' value={0}/>
+                                    </Grid>
+                                    <Grid item xs={5}>
+                                        <Typography variant="h5" component="div" sx={{ color: 'black', marginTop: 8, marginLeft: 3 }}>
+                                            R$ {valor === 0 ? '00.00' : valor}
+                                        </Typography>
+                                    </Grid>
+                                </Grid>
+                                <Button variant="contained" onClick={() => setValor(valor)}>Calcular</Button>
                             </CardContent>
                         </Card>
                     </Grid>
