94a235c200b2dd03af362355c08962971238e237
claudiney63 <claudineyryan63@gmail.com>
2022-08-24 21:03:01 -0300

    função para impedir NaN e numeros negativos

--- calculadora_gorjeta/src/components/CalcGorjeta.jsx
+++ calculadora_gorjeta/src/components/CalcGorjeta.jsx
@@ -9,13 +9,18 @@ import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 
-import './CalcGorjeta.css'
-
 export default function CalcGorjeta() {
 
     const [valor, setValor] = useState(0.00)
     const [comanda, setComanda] = useState(0.00)
     const [gorjeta, setGorjeta] = useState(0.00)
+
+    //função para impedir a entrada de numeros negativos, tornando eles 0
+    //caso sejam digitados
+    const numberNegative = (e, set) => {
+        isNaN(e.target.value) ? set(0) :
+        (e.target.value < 0 ? e.target.value = 0 : set(e.target.value))
+    }
 
     return (
         <>
@@ -32,13 +37,16 @@ export default function CalcGorjeta() {
                                 <Grid container spacing={1}>
                                     <Grid item xs={7}>
                                             <TextField id="outlined-basic" label="Comanda" name='Comanda' margin='normal' 
-                                                variant="outlined" type='number' onChange={(e) => setComanda(e.target.value)}/>
+                                                variant="outlined" type='number' onChange={(e) => numberNegative(e, setComanda)}/>
                                             <TextField id="outlined-basic" label="Gorjeta" name='Gorjeta' margin='normal' 
-                                                variant="outlined" type='number' onChange={(e) => setGorjeta(e.target.value)}/>
+                                                variant="outlined" type='number' onChange={(e) => numberNegative(e, setGorjeta)}/>
                                     </Grid>
                                     <Grid item xs={5}>
-                                        <Typography variant="h5" component="div" sx={{ color: 'black', marginTop: 8, marginLeft: 3 }}>
-                                            R$ {valor === 0 ? '00.00' : valor}
+                                    <Typography variant="h5" component="div" sx={{ color: 'black', marginTop: 3, marginLeft: 1 }}>
+                                            O garçom receberá:
+                                        </Typography>
+                                        <Typography variant="h5" component="div" sx={{ color: 'black', marginTop: 2, marginLeft: 1 }}>
+                                            R$ {valor === 0 || isNaN(valor) ? '00.00' : valor}
                                         </Typography>
                                     </Grid>
                                 </Grid>
